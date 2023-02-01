bab6ba5d21ee78d49afb7a4c62f108c87307c904
elchinbaba <babalielchin@gmail.com>
2023-02-01 13:53:56 +0400

    the version it works good but slow

--- modified_de.py
+++ modified_de.py
@@ -1,86 +1,72 @@
-from scipy.optimize._differentialevolution import differential_evolution, DifferentialEvolutionSolver
+from scipy.optimize import differential_evolution
+import numpy
 
-# FuzzySet, FILE, DE (DE.Problem, DE.Optimizer, DE.SearchControls, DE.BestStrategy, DE.Ranges, DE.RandStrategy) class ve ya tipleri ZFIS.cs faylinda yoxdur
-
+numpy.seterr(divide='ignore', invalid='ignore')
 class Function:
-    def __init__(self, c, bounds, A_ub=None, b_ub=None) -> None:
+    def __init__(self, c, bounds, A_gt=None, b_gt=None, A_lt=None, b_lt=None, A_eq=None, b_eq=None) -> None:
         self.c = c
         self.bounds = bounds
-        self.A_ub = A_ub
-        self.b_ub = b_ub
         self.n = len(c)
-        self.m = len(b_ub)
+        self.A_gt = A_gt
+        self.b_gt = b_gt
+        self.A_lt = A_lt
+        self.b_lt = b_lt
+        self.A_eq = A_eq
+        self.b_eq = b_eq
 
     def f(self, x):
-        summation_base = 0
+        if self.A_eq:
+            for j in range(len(self.A_eq)):
+                sum_A_eq = 0
+                for index in range(self.n):
+                    sum_A_eq += self.A_eq[j][index] * x[index]
 
-        summation_base += (4*x[0] + x[1])
-        summation_base += (3*x[0] + x[1] - 3)
-        summation_base += (4*x[0] + 3*x[1] - x[2] - 6)
-        summation_base += (x[0] + 2*x[1] + x[3] - 4)
+                # If equality constraint (a_j*x = b_j) does not hold then return such a big value that
+                # does not optimize solution and forces the program to continue to iterate
+                if not abs(sum_A_eq - self.b_eq[j]) < 0.01:
+                    return numpy.inf
 
-        # summation = 0
-        # for index in range(self.n):
-        #     summation += self.c[index] * x[index]
-        # summation_base += summation ** 2
+        if self.A_gt:
+            for j in range(len(self.A_gt)):
+                sum_A_gt = 0
+                for index in range(self.n):
+                    sum_A_gt += self.A_gt[j][index] * x[index]
 
-        # for j in range(self.m):
-        #     summation = 0
-        #     for index in range(self.n):
-        #         summation += self.A_ub[j][index] * x[index]
-        #     summation -= self.b_ub[j]
-        #     summation_base += summation ** 2
-        
-        # for index in range(self.n):
-        #     summation_base += self.c[index] * x[index]
+                # If inequality constraint (a_j*x >= b_j) does not hold then return such a big value that
+                # does not optimize solution and forces the program to continue to iterate
+                if not sum_A_gt >= self.b_gt[j]:
+                    return numpy.inf
 
-        # for j in range(self.m):
-        #     for index in range(self.n):
-        #         summation_base += self.A_ub[j][index] * x[index]
-        #     summation_base -= self.b_ub[j]
+        if self.A_lt:
+            for j in range(len(self.A_lt)):
+                sum_A_lt = 0
+                for index in range(self.n):
+                    sum_A_lt += self.A_lt[j][index] * x[index]
 
-        # summation_base **= 2
+                # If inequality constraint (a_j*x <= b_j) does not hold then return such a big value that
+                # does not optimize solution and forces the program to continue to iterate
+                if not sum_A_lt <= self.b_lt[j]:
+                    return numpy.inf
 
-        # summation = 0
-        # for index in range(self.n):
-        #     summation += self.c[index] * x[index]
-        # summation_base += summation ** 2
+        fun = 0
+        for index in range(self.n):
+            fun += self.c[index] * x[index]
 
-        # summation = 0
-        # for j in range(self.m):
-        #     for index in range(self.n):
-        #         summation += self.A_ub[j][index] * x[index]
-        #     summation -= self.b_ub[j]
-        # summation_base += summation ** 2
+        return fun
 
-        return summation_base
+function = Function(c=[4, 1], bounds=[(-10, 10) for _ in range(2)], A_eq=[[3, 1]], b_eq=[3],
+    A_gt=[[4, 3]], b_gt=[6], A_lt=[[1, 2]], b_lt=[4])
 
-function = Function(c=[4, 1, 0, 0], bounds=[(0.1, 1), (1, 2), (0, 0.0001), (0, 0.0001)], A_ub=[
-    [3, 1, 0, 0],
-    [4, 3, -1, 0],
-    [1, 2, 0, 1]
-], b_ub=[3, 6, 4])
-
+print(differential_evolution(func=function.f, bounds=function.bounds, popsize=150, maxiter=5000))
 
 def f(x):
-    summation_base = 0
+    if abs(3*x[0] + x[1] - 3) > 0.01:
+        return numpy.inf
+    if 4*x[0] + 3*x[1] < 6:
+        return numpy.inf
+    if x[0] + 2*x[1] > 4:
+        return numpy.inf
 
-    # summation_base += (4*x[0] + x[1]) ** 2
-    # summation_base += (3*x[0] + x[1] - 3) ** 2
-    # summation_base += (4*x[0] + 3*x[1] - x[2] - 6) ** 2
-    # summation_base += (x[0] + 2*x[1] + x[3] - 4) ** 2
+    return 4*x[0] + x[1]
+# print(differential_evolution(func=f, bounds=[(-10, 10), (-10, 10)], popsize=100, maxiter=2000))
 
-    summation_base += (x[4] + x[5] + x[6]) ** 2
-    summation_base += (3*x[0] + x[1] + x[4] - 3) ** 2
-    summation_base += (4*x[0] + 3*x[1] - x[2] + x[5] - 6) ** 2
-    summation_base += (x[0] + 2*x[1] + x[3] + x[6] - 4) ** 2
-
-    return summation_base
-
-bounds = [(0, 4), (0, 4), (0, 0.001), (0, 0.001), (0, 0.001), (0, 0.001), (0, 0.001)]
-
-print(differential_evolution(func=f, bounds=bounds, popsize=100, maxiter=2000))
-
-# differential_evolution_solver = DifferentialEvolutionSolver(func=function.f, bounds=function.bounds, popsize=50)
-
-# print(differential_evolution_solver.solve())
